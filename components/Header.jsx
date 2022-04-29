0de8c33e689e92f7cb122df2245b07dd00a4e2e1
Piotr Bania <piotr.bania@europe.com>
2022-04-29 15:39:04 +0100

    init

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,11 @@ const Header = () => {
             <div className='md:float-left block'>
                 <Link href="/">
                     <span className='cursor-pointer font-bold text-4xl text-white'>
-                        Cryptoffee
+                        <img
+                        src='cryptoffee_logo.svg'
+                        height="60px"
+                        width="60px"
+                        />
                     </span>
                 </Link>
             </div>
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -12,7 +12,7 @@ const PostCards = ({ post }) => {
                     className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"                    
                 />
             </div>
-            <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
+            <h1 className='transition duration-700 text-center m-5 cursor-pointer hover:text-indigo-800 text-3xl font-semibold'>
                 <Link href={'/post/${post.slug'}>
                     {post.title}
                 </Link>
@@ -31,7 +31,7 @@ const PostCards = ({ post }) => {
                     </p>
                 </div>
                 <div className='font-medium text-gray-700'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
                     <span>
@@ -44,7 +44,7 @@ const PostCards = ({ post }) => {
             </p>
             <div className='text-center'>
                 <Link href={'/post/${post.slug}'}>
-                    <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+                    <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-indigo-700 hover:bg-indigo-600 font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
                         Continue Reading
                     </span>
                 </Link>
